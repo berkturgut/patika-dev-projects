83f3bbc161e19acf2b41ea94632f3d0dce7f7647
berkturgut@outlook.com <berkturgut@outlook.com>
2023-01-23 10:56:17 +0300

    if else if and ternary if notes added.

---
+++ csharp-101/if-elseif-ternaryif/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace if_elseif_ternaryif;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if(time >= 6 && time < 11 )
+        Console.WriteLine("Günaydın");
+
+        else if(time <= 18)
+        Console.WriteLine("İyi günler");
+
+        else
+        Console.WriteLine("İyi geceler");
+
+
+        //Ternary struct example
+        string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
+
+        //Ternary inside Ternary. Different version of above the code
+        sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi günler" : "İyi geceler";
+
+        Console.WriteLine(sonuc); 
+    }
+}
