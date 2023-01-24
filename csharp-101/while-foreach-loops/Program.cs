322e252dba3c267c7d2b3653f1ae6d083faa3284
berkturgut@outlook.com <berkturgut@outlook.com>
2023-01-24 09:22:32 +0300

    whie and foreach loops notes created.

---
+++ csharp-101/while-foreach-loops/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace while_foreach_loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Find the avarage from 1 to input number
+        Console.WriteLine("Lütfen bir sayı giriniz.");
+        int avarageNumber = int.Parse(Console.ReadLine());
+        int counter = 1;
+        int sum = 0;
+        
+        while(counter <= avarageNumber)
+        {
+            sum += counter;
+            counter++;
+        }
+
+        Console.WriteLine(sum/avarageNumber);
+
+
+        //Write all characters from a to z.
+        char character = 'a';
+
+        while(character <= 'z')
+        {
+            Console.Write(character);
+            character++;
+        }
+        
+        //For each
+        Console.WriteLine("****Foreach Loop****");
+        string[] cars = {"BMV","Ford","Nissan"};
+
+        foreach(var singleCarName in cars)
+        {
+            Console.WriteLine(singleCarName);
+        }
+    }
+}
