8fcd2257c6a1ab81d2c66545a5701086504d246f
berkturgut@outlook.com <berkturgut@outlook.com>
2023-02-02 17:12:12 +0300

    Beta version added.

--- csharp-101/consoleapp-homework-csharp/Program.cs
+++ csharp-101/consoleapp-homework-csharp/Program.cs
@@ -3,116 +3,239 @@ class Program
 {
     static void Main(string[] args)
     {
-        //User History
-//     1 ) Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
-//     Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
-//     Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
-
-// 2) Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m).
-// Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
-//Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
-
-
-// 3) Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
-//Sonrasında kullanıcıdan n adet kelime girmesi isteyin.
-//Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
-// Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
-Console.WriteLine("**** Multi Purpose Calculator Console App**** \n");
-Console.WriteLine("*************************");
-Console.WriteLine("1. Find even numbers");
-Console.WriteLine("2. Find dividend");
-Console.WriteLine("3. Find total number of words and characters in a sentence.");
-Console.WriteLine("*************************");
-
-
-for(int i = 0;;i++)
+        UserInterFace.HomeScreen();
+        UserInterFace.Start();
+    }
+}
+public static class UserInterFace
 {
-    Console.Write("Please select an action using sequence number :");
-    bool isActionNumber = byte.TryParse(Console.ReadLine(),out byte actionNumber);
-    
-    if(!isActionNumber || actionNumber < 0 || actionNumber > 3)
-        {
-        
+    public static void HomeScreen()
+    {
 
-        for(int j = 0;;j++)
-        {
-            Console.WriteLine("İnput error. If you want to try again write 'Y' for yes and 'N' for no");
-            string yesNoAnswer = Console.ReadLine();
+        Console.WriteLine("******************************");
+        Console.WriteLine("\n MULTI PURPOSE CALCULATOR \n");
+        Console.WriteLine("******************************");
+        Console.WriteLine("1. Find Even Numbers");
+        Console.WriteLine("2. Find Dividend");
+        Console.WriteLine("3. Word Sorter");
+        Console.WriteLine("4. Word and Character Counter.");
+        Console.WriteLine("******************************");
 
-            if(yesNoAnswer == "Y")
-            break;
-        
-            else if(yesNoAnswer == "N")
+    }
+    public static void Start()
+    {
+
+        Console.Write("\nPlease select an action using sequence number: ");
+        bool isActionNumber = byte.TryParse(Console.ReadLine(), out byte actionNumber);
+
+        if (!isActionNumber || actionNumber < 0 || actionNumber > 4)
+        {
+            Console.WriteLine("You entered invalid action number. Please run the program and try again.");
+        }
+
+        else
+        {
+            switch (actionNumber)
             {
-            Console.WriteLine("Program will shut down.");
-            
+                case (int)ActionsName.Credit:
+                    UserInterFace.CreditScreen();
+                    break;
+                case (int)ActionsName.FindEvenNumbers:
+                    ActionMethods.FindEven();
+                    break;
+                case (int)ActionsName.FindDividend:
+                    ActionMethods.FindDividend();
+                    break;
+                case (int)ActionsName.WordSorter:
+                    ActionMethods.WordSorter();
+                    break;
+                case (int)ActionsName.WordAndCharacterCounter:
+                    ActionMethods.WordAndCharacterCounter();
+                    break;
+            }
+        }
+    }
+    public static void FindEvenNumbersInputScreen()
+    {
+        Console.WriteLine("\n **** Find Even Numbers **** \n");
+        Console.Write("Please enter a length of numbers: ");
+    }
+    public static void InvalidInputScreen()
+    {
+        Console.WriteLine("Input error. Program will shut down");
+        Environment.Exit(0);
+    }
+
+    public static void DuplicateInputScreen()
+    {
+        Console.WriteLine("Duplicate Input error. Program will shut down");
+        Environment.Exit(0);
+    }
+    public static void CreditScreen()
+    {
+        Console.WriteLine("This program created for Patika.Dev's .Net Core Learning Path");
+    }
+}
+enum ActionsName
+{
+    Credit,
+    FindEvenNumbers,
+    FindDividend,
+    WordSorter,
+    WordAndCharacterCounter
+}
+public static class ActionMethods
+{
+    public static void FindEven()
+    {
+        UserInterFace.FindEvenNumbersInputScreen();
+        bool islengtNumbers = int.TryParse(Console.ReadLine(), out int lengthNumber);
+
+        if (!islengtNumbers || lengthNumber <= 0)
+        {
+            UserInterFace.InvalidInputScreen();
+        }
+
+        int[] numbersArray = new int[lengthNumber];
+
+        for (int i = 0; i < lengthNumber; i++)
+        {
+            try
+            {
+                Console.Write($"Please enter {i + 1}. number: ");
+                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (System.Exception)
+            {
+                UserInterFace.InvalidInputScreen();
+            }
+
+        }
+
+        Console.Write("\nNumber value received. Even numbers are: ");
+
+        foreach (var item in numbersArray)
+        {
+            if (item.IsEvenNumber())
+                Console.Write(item + " ");
+        }
+
+    }
+    public static void FindDividend()
+    {
+
+        Console.Write("Please enter total length of numbers: ");
+        bool isLengthNumber = int.TryParse(Console.ReadLine(), out int lengthNumber);
+        Console.Write("Please enter a divider number: ");
+        bool isDividerNumber = int.TryParse(Console.ReadLine(), out int dividerNumber);
+
+        if (lengthNumber > 0 && dividerNumber > 0)
+        {
+            Console.WriteLine($"Please enter {lengthNumber} numbers: ");
+        }
+
+        else
+        {
+            UserInterFace.InvalidInputScreen();
+        }
+
+        int[] dividendNumbers = new int[lengthNumber];
+
+        for (int i = 0; i < lengthNumber; i++)
+        {
+            Console.Write($"Please write {i + 1}. number: ");
+            bool isArrayNumber = int.TryParse(Console.ReadLine(), out int arrayNumber);
+
+            if (arrayNumber > 0 && dividendNumbers.IsArrayItemsDuplicate(arrayNumber))
+            {
+                dividendNumbers[i] = arrayNumber;
             }
 
             else
             {
-            Console.WriteLine("İnput value error.");
-            continue;
+                UserInterFace.DuplicateInputScreen();
             }
+
         }
 
-        continue;
-       
+        Console.WriteLine($"This numbers are dividable by or equal to {dividerNumber}");
+        
+        foreach (var item in dividendNumbers)
+        {
+            if (item.IsEqualOrDividable(dividerNumber))
+                Console.Write(item + " ");
         }
-       
-    else
+
+    }
+    public static void WordSorter()
     {
+        Console.WriteLine("Please enter how many words do you want to enter");
+        bool isWordCount = int.TryParse(Console.ReadLine(), out int wordCount);
 
-        switch(actionNumber)
+        if (isWordCount && wordCount > 0)
         {
-            case 1 : 
-            myMethods.FindEven();
-            break;
-            case 2 :
-            Console.WriteLine("action 2");
-            break;
-            case 3 : 
-            Console.WriteLine("action 3");
-            break;
+            Console.WriteLine($"Please enter {wordCount} words: ");
         }
 
-        break;
+        string[] wordsArray = new string[wordCount];
+
+        for (int i = 0; i < wordCount; i++)
+        {
+            wordsArray[i] = Console.ReadLine();
+        }
+
+        Console.Write("Your words list are: ");
+        for (int i = 0; i < wordCount; i++)
+        {
+            Console.WriteLine(wordsArray[i] + " ");
+        }
+        
+        Console.Write("The reverse of the list are: ");
+        for (int i = 1; i <= wordCount; i++)
+        {
+            Console.WriteLine(wordsArray[wordCount - i] + " ");
+        }
 
     }
-        
-}
+    public static void WordAndCharacterCounter()
+    {
+        Console.WriteLine("Please enter your sentence");
+        string sentence = Console.ReadLine();
+        sentence.WordsCounter();
+        sentence.CharacterCounter();
 
     }
 }
-
-public static class myMethods
+public static class ActionExtensionClass
 {
-
-public static void FindEven()
-{
-    Console.Write("Please enter a number: ");
-    
-    bool islengtNumbers = int.TryParse(Console.ReadLine(),out int lengthNumber );
-    
-        
-        if(islengtNumbers)
-         {
-        Console.WriteLine($"Please enter {lengthNumber} different numbers");
-         }
-        
+    public static bool IsEvenNumber(this int value)
+    {
+        if (value % 2 == 0)
+            return true;
         else
-        {
-         Console.WriteLine("Input error. Program will shut down");
-           
-        }
-   
-    
-
-
-
-
-}
-
-
-
+            return false;
+    }
+    public static bool IsEqualOrDividable(this int value, int divider)
+    {
+        if (value % divider == 0 || value == divider)
+            return true;
+        else
+            return false;
+    }
+    public static bool IsArrayItemsDuplicate(this int[] words, int item)
+    {
+        bool result = Array.Exists(words, element => element == item) ? false : true;
+        return result;
+    }
+    public static void WordsCounter(this string sentence)
+    {
+        string[] words = sentence.Split(" ");
+        Console.WriteLine($"The total numbers of words of the sentence are: {words.Length}");
+    }
+    public static void CharacterCounter(this string sentence)
+    {
+        int numberOfWords = sentence.Replace(" ", "").Length;
+        Console.WriteLine($"The total number of characters in sentences are: {numberOfWords}");
+    }
 
 }
