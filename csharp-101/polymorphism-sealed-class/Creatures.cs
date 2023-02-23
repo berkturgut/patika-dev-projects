4084b5c643460a6aaba8c54c0665aeb916777800
berkturgut@outlook.com <berkturgut@outlook.com>
2023-02-23 09:15:44 +0300

    polymorphism, virtual and override method and sealed class notes added.

---
+++ csharp-101/polymorphism-sealed-class/Creatures.cs
@@ -0,0 +1,25 @@
+namespace inheritance;
+//public sealed class Creatures : you can't use inheritance anymore with sealed.
+
+public class Creatures
+{
+
+protected void Feed(){
+    Console.WriteLine("Creatures feed.");
+}
+
+public void aspiration(){
+    Console.WriteLine("Creatures aspirate.");
+}
+
+public void excretion(){
+    Console.WriteLine("Creatures excrete.");
+}
+
+//This is a virtual method and we can use sub class with override method. Also we can change the content.
+public virtual void ReactionBehaviour()
+{
+    Console.WriteLine("Creatures reaction to the event.");
+}
+
+}
